d6c3d5430356e6d91fc1e330ef0d4cc77cc13b2c
Leona Istrefi <145564029+LeonaIstrefi@users.noreply.github.com>
2024-03-10 16:17:45 +0100

    Updated Author.java
    I have finished my first file Author.java. After i added a few annotations (and inform myself on how to use them properly) i have added them into my code. I have also added attributes into my code like "Id" or "birthday"

--- uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/author/Author.java
+++ uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/author/Author.java
@@ -1,14 +1,30 @@
 package ch.noseryoung.uek2951LeIsAuthor.domain.author;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@Table
+@Table (name = "Author")
 public class Author {
+    @Id
+
+    @NotNull
+    private Integer Id;
+
+    private String birthday;
+
+    private String Name;
+
+    @Column
+    private String ProfilePicture;
+
+
 }

b7f8b35baadd67be1f2d6f08ea272583269e5015
Leona Istrefi <145564029+LeonaIstrefi@users.noreply.github.com>
2024-03-13 13:30:51 +0100

    Here i added all my information into the table.

---
+++ uek295-1-LeIs-Author/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO author (author_id, name, birthday, profile_picture)
+VALUES (1, 'Ana Huang', '7. March 1991', 'www.author.com' ),
+       (2, 'J.K. Rowling', '31. Juli 1965', 'www.author.com'),
+       (3, 'Jennifer Lynn Barnes', '19. Oktober 1984','www.author.com' ),
+       (4, 'Holly Black', '10. November 1971','www.author.com' ),
+       (5, 'Friedrich Duerrenmatt', '5. Januar 1921','www.author.com' ),
+       (6, 'Stephen King', '21. September 1947','www.author.com' ),
+       (7, 'William Shakespeare', '26. April 1564', 'www.author.com' ),
+       (8, 'Charles Dickens', '7. Februar 1812','www.author.com' ),
+       (9, 'Sarah J. Maas','5. März 1986', 'www.author.com'),
+       (10, 'Rick Riordan', '5. Juni 1964','www.author.com' );
+
+

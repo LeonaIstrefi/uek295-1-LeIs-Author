e23c7b062c25b2dd078c19317917ec9db7d80862
Leona Istrefi <145564029+LeonaIstrefi@users.noreply.github.com>
2024-03-13 22:19:24 +0100

    i had a few small mistakes in my code, which i had to fix

--- uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/user/User.java
+++ uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/user/User.java
@@ -1,4 +1,32 @@
 package ch.noseryoung.uek2951LeIsAuthor.domain.user;
 
+
+import ch.noseryoung.uek2951LeIsAuthor.domain.authority.Authority;
+import ch.noseryoung.uek2951LeIsAuthor.domain.role.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "author_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "id_role", referencedColumnName = "role_id")
+    private Role userRole;
 }

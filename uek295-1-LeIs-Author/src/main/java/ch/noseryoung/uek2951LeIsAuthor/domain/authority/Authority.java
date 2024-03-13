04d0a3b803ee60c515d54da9cc35feec33746555
Leona Istrefi <145564029+LeonaIstrefi@users.noreply.github.com>
2024-03-13 14:51:18 +0100

    i wrote my Authority.java file

--- uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/authority/Authority.java
+++ uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/authority/Authority.java
@@ -1,4 +1,19 @@
 package ch.noseryoung.uek2951LeIsAuthor.domain.authority;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Authority")
 public class Authority {
+    @Id
+    @Column(name = "authority_id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
 }

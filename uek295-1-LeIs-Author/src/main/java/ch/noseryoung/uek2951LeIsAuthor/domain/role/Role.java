3e1fad02965196ffffc8831c960a9297c32dc6be
Leona Istrefi <145564029+LeonaIstrefi@users.noreply.github.com>
2024-03-13 22:17:16 +0100

    i implemented my entire role file

--- uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/role/Role.java
+++ uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/role/Role.java
@@ -1,4 +1,31 @@
 package ch.noseryoung.uek2951LeIsAuthor.domain.role;
 
+
+import ch.noseryoung.uek2951LeIsAuthor.domain.authority.Authority;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "role")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "author_role_authority",
+            joinColumns = @JoinColumn(name = "id_role", referencedColumnName = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "id_authority", referencedColumnName = "authority_id"))
+    private Set<Authority> authoritySet;
 }

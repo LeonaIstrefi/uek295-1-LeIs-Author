bbb846a1791bfb7d4cc2f6980cce725d339254e6
Leona Istrefi <145564029+LeonaIstrefi@users.noreply.github.com>
2024-03-13 22:39:50 +0100

    In this file i have added my Exceptions and my public class for it

--- uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/author/AuthorController.java
+++ uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/author/AuthorController.java
@@ -1,8 +1,10 @@
 package ch.noseryoung.uek2951LeIsAuthor.domain.author;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,16 +20,25 @@ public class AuthorController {
     private AuthorService service;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('READ')")
+    @Operation(summary = "get all Authors",
+            description = "With this endpoint you get all Authors")
     public ResponseEntity<List<Author>> getAuthor() {
         return ResponseEntity.ok().body(service.getAuthors());
     }
 
     @GetMapping("/{authorID}")
-    public ResponseEntity<Author> readAuthor(@PathVariable("authorID") Integer authorID) {
+    @PreAuthorize("hasAuthority('READ')")
+    @Operation(summary = "get one specific Auther by id",
+            description = "With this endpoint, you get one specific Author by id")
+    public ResponseEntity<Author> readAuthor(@PathVariable("authorID") Integer authorID) throws AuthorNotFoundException {
         return ResponseEntity.ok().body(service.getAuthor(authorID));
     }
 
     @PostMapping("/")
+    @PreAuthorize("hasAuthority('CREATE')")
+    @Operation(summary = "create Author",
+            description = "Creates a new Author")
     public ResponseEntity<Author> createAuthor(@RequestBody Author newauthor) throws InstanceAlreadyExistsException {
         return ResponseEntity.status(201).body(service.createAuthor(newauthor));
 
@@ -35,15 +46,36 @@ public class AuthorController {
     }
 
     @PutMapping("/{authorID}")
+    @PreAuthorize("hasAuthority('UPDATE')")
+    @Operation(summary = "updates Authors",
+            description = "It updates Authors by id")
     public ResponseEntity<Author> updateAuthor(@PathVariable("authorID")int authorID, @RequestBody Author author) throws InstanceNotFoundException {
         return ResponseEntity.status(200).body(service.updateAuthor(authorID, author));
 
     }
 
     @DeleteMapping("/{authorID}")
-    public String deleteAuthor(@PathVariable("authorID") Integer AuthorId) {
+    @PreAuthorize("hasAuthority('DELETE')")
+    @Operation(summary = "Delete Author",
+            description = "Deletes Auther by id")
+    public String deleteAuthor(@PathVariable("authorID") Integer AuthorId) throws AuthorNotFoundException {
         service.deleteAuthor(AuthorId);
         return "The id " + AuthorId + " has been deleted ";
+    }
+
+    public class AuthorNotFoundException extends Exception{
+        public AuthorNotFoundException(String message){
+            super(message);
+        }
+
+        public AuthorNotFoundException() {
+            super();
+        }
+    }
+
+    @ExceptionHandler(AuthorNotFoundException.class)
+    public ResponseEntity<String> handleNoSuchElementException(AuthorNotFoundException pnfe) {
+        return ResponseEntity.status(404).body(pnfe.getMessage());
     }
 
 }

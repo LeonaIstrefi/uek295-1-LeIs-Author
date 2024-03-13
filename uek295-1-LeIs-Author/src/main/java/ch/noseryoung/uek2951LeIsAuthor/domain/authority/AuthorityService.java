7c4329d260c8d6935ec1f64e28d38592d96d6c85
Leona Istrefi <145564029+LeonaIstrefi@users.noreply.github.com>
2024-03-13 22:16:39 +0100

    I found a more simple way to do my Service, which is why i changed the authority file

--- uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/authority/AuthorityService.java
+++ uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/authority/AuthorityService.java
@@ -1,9 +1,12 @@
 package ch.noseryoung.uek2951LeIsAuthor.domain.authority;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthorityService {
-    private AuthorityRepository authorityRepository;
+
+   @Autowired
+    AuthorityRepository repository;
 }
--- uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/role/RoleService.java
+++ uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/role/RoleService.java
@@ -1,4 +1,13 @@
 package ch.noseryoung.uek2951LeIsAuthor.domain.role;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RoleService {
+
+    @Autowired
+    RoleRepository repository;
 }

722fee9c757c3c4a77e757a721921c52fc808302
Leona Istrefi <145564029+LeonaIstrefi@users.noreply.github.com>
2024-03-13 13:29:54 +0100

    I here had to change all the UpperCase letters to lower case letters, because the code could not work with UpperCase letters. I also needed to rename my column Author_id, since it did not match the name i had in my data.sql

--- uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/author/Author.java
+++ uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/author/Author.java
@@ -4,24 +4,41 @@ package ch.noseryoung.uek2951LeIsAuthor.domain.author;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.nio.file.NoSuchFileException;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
-@Table (name = "Author")
+@Table (name = "author")
 public class Author {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     @Id
     @NotNull
+    @Column(name = "author_id")
     private Integer Id;
 
+    @Column(name = "birthday")
     private String birthday;
 
+    @Column(name = "name")
     private String Name;
 
-    @Column
-    private String ProfilePicture;
+    @Column(name = "profile_picture")
+    private String profile_picture;
 
+
+    public Author(Integer id, String birthday, String name, String profile_picture) {
+        setId(id);
+        setBirthday(birthday);
+        setName(name);
+        setProfile_picture(profile_picture);
+    }
 
 }

767accf1b6e8101f11cf7aa0b79ee004b39862d7
Leona Istrefi <145564029+LeonaIstrefi@users.noreply.github.com>
2024-03-13 16:12:30 +0100

    I started to work in my AuthorityService.java file

--- uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/authority/AuthorityService.java
+++ uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/authority/AuthorityService.java
@@ -1,4 +1,9 @@
 package ch.noseryoung.uek2951LeIsAuthor.domain.authority;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthorityService {
+    private AuthorityRepository authorityRepository;
 }

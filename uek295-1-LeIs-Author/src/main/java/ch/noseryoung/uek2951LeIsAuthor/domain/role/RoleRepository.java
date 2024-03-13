93cde5eb7dfb059422b54c4a83e97e9f945565aa
Leona Istrefi <145564029+LeonaIstrefi@users.noreply.github.com>
2024-03-13 22:17:39 +0100

    i added my repository in role

--- uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/role/RoleRepository.java
+++ uek295-1-LeIs-Author/src/main/java/ch/noseryoung/uek2951LeIsAuthor/domain/role/RoleRepository.java
@@ -1,4 +1,9 @@
 package ch.noseryoung.uek2951LeIsAuthor.domain.role;
 
-public class RoleRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
 }
